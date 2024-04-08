7f193b2ce13f8803612d798731adb80c6c970e0b
yana1al <alimasharma@gmail.com>
2024-04-08 11:42:37 -0700

    Deploy Heroku

--- public/javascripts/client.js
+++
@@ -1,6 +0,0 @@
-const token = localStorage.getItem('token');
-const decodedToken = jwt_decode(token); // Assuming you're using a library like 'jwt-decode'
-const currentTime = Math.floor(Date.now() / 1000);
-if (decodedToken.exp < currentTime) {
-  
-}

8d80d106045a6cbb37217b81749a756c73e6f5c7
yana1al <alimasharma@gmail.com>
2024-04-08 12:03:19 -0700

    Final debugging

--- config/passport.js
+++ config/passport.js
@@ -2,10 +2,12 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
-  clientID: '562293342147-tj3b62ouoqdf06b8hogmtg8k5m5q90t7.apps.googleusercontent.com',
-  clientSecret: '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',
-  callbackURL: 'https://yana-s-invitess-c5fb1ede161f.herokuapp.com/oauth2callback'
-}, (accessToken, refreshToken, profile, done) => {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+},
+
+(accessToken, refreshToken, profile, done) => {
   // Verify user and create or retrieve user record in database
 }));
 
